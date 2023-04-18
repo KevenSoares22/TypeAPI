bdce2b0265ccf3eeb5c8bd7520c6ca1e4cc96a17
KevenSoares22 <keven.v.soares@gmail.com>
2023-04-18 19:03:57 -0300

    Configurando rotas e componentes da Home

---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,26 @@
+
+
+import { BsSearch } from 'react-icons/bs';
+
+const Search = () =>{
+
+
+  return(
+    <>
+    
+    <h1>Pesquise um usuario</h1>
+    <input type="text" placeholder="Digite o usuario"/>
+    <button><BsSearch/></button>
+
+
+
+    </>
+
+
+
+  );
+
+}
+
+export default Search;
+
---
+++ src/routes/Home.tsx
@@ -0,0 +1,19 @@
+import Search from "../components/Search/Search";
+
+const Home = () =>{
+
+    return(
+    
+    <>
+      <h1>Home</h1>
+      <Search />
+    
+    </>
+    
+    );
+
+
+}
+
+export default Home;
+
